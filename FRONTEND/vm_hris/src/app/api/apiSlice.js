774cee9239d9d25f6bee86fd139865f9e6bc0173
Hero Legaspi <139465408+HeroreH29@users.noreply.github.com>
2024-01-04 17:01:31 +0800

    Update apiSlice.js

--- FRONTEND/vm_hris/src/app/api/apiSlice.js
+++ FRONTEND/vm_hris/src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://192.168.1.6:3500",
+  baseUrl: "https://viamare-hris-api.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;

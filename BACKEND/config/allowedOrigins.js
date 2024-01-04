3dfc25bf44ecaf9ac785a53ca7c25fdce94d40fd
Hero Legaspi <139465408+HeroreH29@users.noreply.github.com>
2024-01-04 15:27:44 +0800

    Update allowedOrigins.js

--- BACKEND/config/allowedOrigins.js
+++ BACKEND/config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
   "http://localhost:3500",
   "http://localhost:5000",
   "http://192.168.1.6:3000",
+  "https://viamare-hris.onrender.com",
 ];
 
 module.exports = allowedOrigins;

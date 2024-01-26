1af75d7f075d1205e14756c44fa34c17c07e5244
Hero Legaspi <139465408+HeroreH29@users.noreply.github.com>
2024-01-26 14:20:18 +0800

    Update depOptions.js

--- FRONTEND/vm_hris/src/config/depOptions.js
+++ FRONTEND/vm_hris/src/config/depOptions.js
@@ -3,12 +3,16 @@ export const STATUS = {
   Single: "Single",
   Married: "Married",
   "Special Child": "Special Child",
+  "Divorced/Annuled": "Divorced/Annuled",
+  "Widow/Widower": "Widow/Widower",
 };
 
 export const RELATIONSHIP = {
   "Select relationship...": "",
   Son: "Son",
   Daughter: "Daughter",
+  Mother: "Mother",
+  Father: "Father",
 };
 
 export const COVERED = {
